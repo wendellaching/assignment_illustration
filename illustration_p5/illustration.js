6bc31d52161832227c335ec0b9b3e6d4de88048b
wendellaching <vincent23>
2015-10-23 15:33:20 -0400

    saved
    don’t use anvil’s index

--- illustration_p5/illustration.js
+++ illustration_p5/illustration.js
@@ -1,9 +1,31 @@
 // illustration.js
+var backgroundImage;
+var stargroundImage;
+var ghostaImage;
+var ghostbImage;
+var girlImage;
+var moonImage;
+
+function preload() {
+	backgroundImage = loadImage("images/background.jpg");
+	ghostaImage = loadImage("images/ghosta.png");
+	ghostbImage = loadImage("images/ghostb.png");
+	girlImage = loadImage("images/girl.png");
+	moonImage = loadImage("images/moon.png");
+	stargroundImage = loadImage("images/starground/.png");
+}
+
 
 function setup() {
 	createCanvas(800, 600);
 }
 
 function draw() {
-	background(200,0,0);
+	image(backgroundImage, 0, 0);
+	if (mouseIsPressed) {
+		ghosta = mouseY;
+	}
+	if (mouseIsPressed) {
+		ghosta = mouseX;
+	}
 }
